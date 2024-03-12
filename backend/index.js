3be82c12fa98c45f56c34af8c5a66861a9b61b0b
chandima2000 <hychandima2000@gmail.com>
2024-03-12 23:58:44 +0530

    Create a Connection with the MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,24 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+
+
+mongoose.connect(process.env.MONGODB)
+        .then(() => {
+            console.log("DB is connected")
+        })
+        .catch((err) => {
+            console.error(err)
+        });
 
 const app = express();
 
+
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
-})
+});
